ff1f4fdd409cdc6b71d3c1d24c39f6a4c24545c9
Zachary Gaines <zacharygaines28@gmail.com>
2021-05-10 13:36:29 -0700

    finished 06 tests

--- src/object-keys-2.js
+++ src/object-keys-2.js
@@ -12,3 +12,33 @@ export function updateNumbers(obj) {
   });
   return array;
 }
+export function totalCharacters(arr) {
+  let count = 0;
+  arr.forEach((item) => {
+    if (item.name) count++;
+    if (item.spouse) count++;
+    if (item.children) count = count + item.children.length;
+  });
+  return count;
+
+};
+
+export function hasChildrenEntries(arr, character) {
+  let hasChildren = false;
+  arr.forEach((item) => {
+    if (item.name === character) {
+      if (item.children.length > 0) return hasChildren = true;
+    };
+  });
+  return hasChildren;
+}
+
+export function sortByChildren(arr) {
+  const anArray = arr.sort((a, b) => {
+    return a.children.length - b.children.length;
+  })
+    .sort((a, b) => {
+      return a.name - b.name;
+    });
+  return anArray;
+};
--- src/object-keys-2.test.js
+++ src/object-keys-2.test.js
@@ -1,4 +1,4 @@
-import { getHouses, updateNumbers } from './object-keys-2.js';
+import { getHouses, updateNumbers, sortByChildren, hasChildrenEntries, totalCharacters } from './object-keys-2.js';
 test('it gets the names of the houses', () => {
   const characters = [
     {
@@ -52,4 +52,155 @@ test('it changes objects into an array of strings', () => {
   const object = { 'Grace Hopper': '222-303-5938', 'Ada Lovelace': '222-349-9842', 'Alan Turing': '222-853-5933' };
   const output = updateNumbers(object);
   expect(output).toEqual(['Grace Hopper: 222-303-5938', 'Ada Lovelace: 222-349-9842', 'Alan Turing: 222-853-5933']);
+});
+
+test('return the total number of characters in the data array', () => {
+  const input = [
+    {
+      name: 'Eddard',
+      spouse: 'Catelyn',
+      children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+      house: 'Stark'
+    },
+    {
+      name: 'Jon A.',
+      spouse: 'Lysa',
+      children: ['Robin'],
+      house: 'Arryn'
+    },
+    {
+      name: 'Cersei',
+      spouse: 'Robert',
+      children: ['Joffrey', 'Myrcella', 'Tommen'],
+      house: 'Lannister'
+    },
+    {
+      name: 'Daenarys',
+      spouse: 'Khal Drogo',
+      children: ['Drogon', 'Rhaegal', 'Viserion'],
+      house: 'Targaryen'
+    },
+    {
+      name: 'Mace',
+      spouse: 'Alerie',
+      children: ['Margaery', 'Loras'],
+      house: 'Tyrell'
+    },
+    {
+      name: 'Euron',
+      spouse: null,
+      children: [],
+      house: 'Greyjoy'
+    },
+    {
+      name: 'Jon S.',
+      spouse: null,
+      children: [],
+      house: 'Snow'
+    }
+  ];
+
+  const output = totalCharacters(input);
+  expect(output).toEqual(26);
+});
+
+test('Given an arr of objects and a target/character, the function will check if the character has children then return a boolean value of either true or false.', () => {
+  const characters = [
+    {
+      name: 'Eddard',
+      spouse: 'Catelyn',
+      children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+      house: 'Stark'
+    },
+    {
+      name: 'Jon A.',
+      spouse: 'Lysa',
+      children: ['Robin'],
+      house: 'Arryn'
+    },
+    {
+      name: 'Cersei',
+      spouse: 'Robert',
+      children: ['Joffrey', 'Myrcella', 'Tommen'],
+      house: 'Lannister'
+    },
+    {
+      name: 'Daenarys',
+      spouse: 'Khal Drogo',
+      children: ['Drogon', 'Rhaegal', 'Viserion'],
+      house: 'Targaryen'
+    },
+    {
+      name: 'Mace',
+      spouse: 'Alerie',
+      children: ['Margaery', 'Loras'],
+      house: 'Tyrell'
+    },
+    {
+      name: 'Euron',
+      spouse: null,
+      children: [],
+      house: 'Greyjoy'
+    },
+    {
+      name: 'Jon S.',
+      spouse: null,
+      children: [],
+      house: 'Snow'
+    }
+  ];
+  const output = sortByChildren(characters); //act
+  expect(output).toEqual([{ name: 'Euron', spouse: null, children: [], house: 'Greyjoy' }, { name: 'Jon S.', spouse: null, children: [], house: 'Snow' }, { name: 'Jon A.', spouse: 'Lysa', children: ['Robin'], house: 'Arryn' }, { name: 'Mace', spouse: 'Alerie', children: ['Margaery', 'Loras'], house: 'Tyrell' }, { name: 'Cersei', spouse: 'Robert', children: ['Joffrey', 'Myrcella', 'Tommen'], house: 'Lannister' }, { name: 'Daenarys', spouse: 'Khal Drogo', children: ['Drogon', 'Rhaegal', 'Viserion'], house: 'Targaryen' }, { name: 'Eddard', spouse: 'Catelyn', children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'], house: 'Stark' }]);
+
+});
+
+
+
+test('You will have an array of objects which you will sort through. The houses are sorted by amount of children ', () => {
+  const characters = [
+    {
+      name: 'Eddard',
+      spouse: 'Catelyn',
+      children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+      house: 'Stark'
+    },
+    {
+      name: 'Jon A.',
+      spouse: 'Lysa',
+      children: ['Robin'],
+      house: 'Arryn'
+    },
+    {
+      name: 'Cersei',
+      spouse: 'Robert',
+      children: ['Joffrey', 'Myrcella', 'Tommen'],
+      house: 'Lannister'
+    },
+    {
+      name: 'Daenarys',
+      spouse: 'Khal Drogo',
+      children: ['Drogon', 'Rhaegal', 'Viserion'],
+      house: 'Targaryen'
+    },
+    {
+      name: 'Mace',
+      spouse: 'Alerie',
+      children: ['Margaery', 'Loras'],
+      house: 'Tyrell'
+    },
+    {
+      name: 'Euron',
+      spouse: null,
+      children: [],
+      house: 'Greyjoy'
+    },
+    {
+      name: 'Jon S.',
+      spouse: null,
+      children: [],
+      house: 'Snow'
+    }
+  ];
+  const output = hasChildrenEntries(characters, 'Eddard'); //act
+  expect(output).toEqual(true);
 });
