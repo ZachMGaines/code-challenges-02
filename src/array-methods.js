6eebf5fe980987ee0c961a840ce874dbd229969c
Zachary Gaines <zacharygaines28@gmail.com>
2021-05-16 22:33:06 -0700

    fixed and tests done

--- src/array-methods.js
+++ src/array-methods.js
@@ -1,20 +1,43 @@
 export function howMuchPencil(str) {
-
+  let arr = [];
+  for (let i = 0; i <= str.length; i++) {
+    arr.push(str.slice(i));
+  }
+  return arr;
 }
 
 export function wordsToCharList(str) {
-
-
+  let arr = [];
+  for (let letter of str) {
+    arr.push(letter);
+  }
+  return arr;
 }
 
 export function listFoods(recipe) {
-
+  let arr = [];
+  recipe.ingredients.map(n => {
+    const split = n.split(' ');
+    const amount = `${split[0]} ${split[1]}`;
+    arr.push(n.replace(`${amount} `, ''));
+    return arr;
+  });
+  return arr;
 }
 
 export function stepActions(recipe) {
-
+  let arr = [];
+  recipe.steps.map(n => {
+    const split = n.split(' ');
+    const action = `${split[0]}`;
+    arr.push(action);
+    return arr;
+  });
+  return arr;
 }
 
-export function removeLastCharacters(str, numOfCharacters) {
-
+export function removeLastCharacters(str, numberOfCharacters) {
+  if (numberOfCharacters > 0) return str.substr(0, str.length - numberOfCharacters);
+  if (numberOfCharacters < 0) return str;
 }
+
