9ac455c3b87f9c351900014afd324c50c8554654
Zachary Gaines <zacharygaines28@gmail.com>
2021-05-17 12:00:42 -0700

    2nd test

--- src/munge-array.js
+++ src/munge-array.js
@@ -1,3 +1,8 @@
 export function returnTen(str) {
   return str.split('').slice(-10);
 }
+
+export function findMax(matrix) {
+  const maxOfEachArray = matrix.map(arr => Math.max.apply(null, arr));
+  return Math.max.apply(null, maxOfEachArray);
+}
