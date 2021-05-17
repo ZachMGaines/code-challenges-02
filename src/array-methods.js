34aeab6ebbc743d726dbf119961a2fb6b13569e0
Zachary Gaines <zacharygaines28@gmail.com>
2021-05-16 19:49:39 -0700

    wrote out test

--- src/array-methods.js
+++ src/array-methods.js
@@ -6,3 +6,15 @@ export function wordsToCharList(str) {
 
 
 }
+
+export function listFoods(recipe) {
+
+}
+
+export function stepActions(recipe) {
+
+}
+
+export function removeLastCharacters(str, numOfCharacters) {
+
+}
